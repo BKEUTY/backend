cf7358fa1fa823a3bdf8d13f418c228010a00c7b
LangTuDaTinh <quangviet210804@gmail.com>
2025-12-28 06:29:32 +0700

    first commit

---
+++ src/main/java/com/hcmut/bkuety/BkuetyApplication.java
@@ -0,0 +1,13 @@
+package com.hcmut.bkuety;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BkuetyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BkuetyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hcmut/bkuety/BkuetyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hcmut.bkuety;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BkuetyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
