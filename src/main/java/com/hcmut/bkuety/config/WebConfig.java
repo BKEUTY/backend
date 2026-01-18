7323cdc66909a03bdae526987c594e353c753b4d
Phạm Thanh Phong <phamthanhphong9u@gmail.com>
2026-01-18 21:05:15 +0700

    fix cors for localhost

--- src/main/java/com/hcmut/bkuety/config/WebConfig.java
+++ src/main/java/com/hcmut/bkuety/config/WebConfig.java
@@ -1,6 +1,5 @@
 package com.hcmut.bkuety.config;
 
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
@@ -14,8 +13,8 @@ public class WebConfig {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/api/**") // Áp dụng cho tất cả API
-                        .allowedOrigins("*") // URL của Frontend
+                registry.addMapping("/**") // Áp dụng cho tất cả API
+                        .allowedOriginPatterns("*") // URL của Frontend
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // Các method cho phép
                         .allowedHeaders("*") // Cho phép tất cả các header
                         .allowCredentials(true); // Cho phép gửi Cookie/Auth Header nếu cần
